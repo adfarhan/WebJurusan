9d7e61652f3be39311bf17b523b260e6d9d1a22e
andiniirianaa <andiniriana1@gmail.com>
2025-01-13 11:35:24 +0700

    commit kedua

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,7 +13,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Beranda</a>
+                    <a class="nav-link" href="{{ route('beranda') }}">Beranda</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Tentang</a>
